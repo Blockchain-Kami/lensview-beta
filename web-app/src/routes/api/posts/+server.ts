5944516c7944fa4401cdee9b95c4c01ec41caa7d
parikshitbarua <parikshitb04@gmail.com>
2023-05-01 19:33:20 +0530

    added logic to fetch comments

--- web-app/src/routes/api/posts/+server.ts
+++ web-app/src/routes/api/posts/+server.ts
@@ -2,6 +2,7 @@ import { PUBLIC_LENS_API_URL } from "$env/static/public";
 import getPosts from '../../../graphql/getPosts';
 import { json } from '@sveltejs/kit';
 import { APP_LENS_ID } from '$env/static/private';
+import getComments from "../../../graphql/getComments";
 
 
 export async function GET(request) {
@@ -9,7 +10,7 @@ export async function GET(request) {
     const hashedURL = request.url.searchParams.get('hashedURL');
 
     try {
-        const data = await fetch(PUBLIC_LENS_API_URL, {
+        const posts = await fetch(PUBLIC_LENS_API_URL, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -23,9 +24,41 @@ export async function GET(request) {
                 }),
             })
 
-        const response = await data.json()
+        const response = await posts.json()
+        const parentPostID = response.data.publications.items[0].id;
+        const sourceURL = response.data.publications.items[0].metadata.description;
 
-        return json(response);
+        console.log(sourceURL);
+
+        try {
+            const comments = await fetch(PUBLIC_LENS_API_URL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    query: getComments,
+                    variables: {
+                        publicationId: parentPostID
+                    },
+                }),
+            })
+
+            const commentsJSON = await comments.json();
+            const commentItems = commentsJSON.data.publications.items;
+
+            const responseComments = {
+                URL: sourceURL,
+                items : commentItems
+            };
+
+
+            return json(responseComments)
+
+        } catch {
+            console.log("error")
+        }
+
     } catch {
         console.log("error");
     }
