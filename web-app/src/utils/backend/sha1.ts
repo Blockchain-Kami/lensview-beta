c14e99542f0d08d166015ad1cb8e6c9ef22daa25
parikshitbarua <parikshitb04@gmail.com>
2023-05-07 05:56:32 +0530

    Removed file: sha1.ts

--- web-app/src/utils/backend/sha1.ts
+++
@@ -1,9 +0,0 @@
-import crypto from 'crypto';
-
-export const createHash = (url: string) => {
-    const hash = crypto.createHash('sha1').update(JSON.stringify(url)).digest('hex')
-    return hash
-}
-
-
-
