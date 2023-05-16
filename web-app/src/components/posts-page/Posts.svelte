e9efecb4a16b52b5ea72b8a9e46a81793762da24
anjaysahoo3 <anjaysahoo3@gmail.com>
2023-05-16 09:18:26 +0530

    Added logic for date formatting in posts date

--- web-app/src/components/posts-page/Posts.svelte
+++ web-app/src/components/posts-page/Posts.svelte
@@ -45,6 +45,56 @@
       return fetchedLensURL;
     }
   };
+
+  /**
+   * It returns the formatted date based on the date passed
+   * If the date is of today, it returns the seconds, minutes or hours
+   * If the date is of 1 month from today, it returns the days
+   * If the date is of 1 year from today, it returns the months
+   * If the date is of more than 1 year from today, it returns the years
+   * @param date
+   */
+  const getFormattedDate = (date) => {
+    const today = new Date();
+    const postDate = new Date(date);
+
+    const diffTime = Math.abs(today.getTime() - postDate.getTime());
+    const oneDay = 24 * 60 * 60 * 1000;
+    const diffDays = Math.floor(diffTime / oneDay);
+
+    if (diffDays === 0) {
+      const diffHours = Math.floor(diffTime / (1000 * 60 * 60));
+      if (diffHours === 0) {
+        const diffMinutes = Math.floor(diffTime / (1000 * 60));
+        if (diffMinutes === 0) {
+          const diffSeconds = Math.floor(diffTime / (1000));
+          return `${diffSeconds} seconds ago`;
+        } else {
+          return `${diffMinutes} minutes ago`;
+        }
+      } else {
+        return `${diffHours} hours ago`;
+      }
+    } else if (diffDays === 1) {
+      return `${diffDays} day ago`;
+    } else if (diffDays < 30) {
+      return `${diffDays} days ago`;
+    } else if (diffDays < 365) {
+      const diffMonths = Math.floor(diffDays / 30);
+      if (diffMonths === 1) {
+        return `${diffMonths} month ago`;
+      } else {
+        return `${diffMonths} months ago`;
+      }
+    } else {
+      const diffYears = Math.floor(diffDays / 365);
+      if (diffYears === 1) {
+        return `${diffYears} year ago`;
+      } else {
+        return `${diffYears} years ago`;
+      }
+    }
+  };
 </script>
 
 
@@ -61,7 +111,7 @@
           <div class="posts__post__data">
             <div class="posts__post__data__header">
               <div class="posts__post__data__header__handle">@{post["profile"]["handle"]}</div>
-              <div class="posts__post__data__header__date">{new Date(post["createdAt"]).toDateString()}</div>
+              <div class="posts__post__data__header__date">{getFormattedDate(post["createdAt"])}</div>
             </div>
             <div class="posts__post__data__content">{post["metadata"]["content"]}</div>
             <div class="posts__post__data__reaction-bar">
