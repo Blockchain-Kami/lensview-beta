b5aa65eb932e1efc0cc2b94a0e47e1963451be93
parikshitbarua <parikshitb04@gmail.com>
2023-05-25 01:59:34 +0530

    using playwright to get screenshot.

--- web-app/src/components/main-page/Feeds.svelte
+++ web-app/src/components/main-page/Feeds.svelte
@@ -1,4 +1,30 @@
 <script lang="ts">
+  import {onMount} from "svelte";
+
+  let imageurl;
+
+  onMount(async () => {
+    let STRING_CHAR;
+    await fetch('/api/get-img', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(async(res) => {
+      const json = await res.json()
+      console.log("RESPONSE", json);
+      const img = new Uint8Array(json["data"]["data"]);
+
+      STRING_CHAR = img.reduce((data, byte) => {
+        return data + String.fromCharCode(byte);
+      }, '');
+
+      let base64String = btoa(STRING_CHAR);
+
+      imageurl = `data:image/jpg;base64,` + base64String;
+    })
+  })
+
 </script>
 
 
@@ -13,6 +39,7 @@
   <div class="main__search-area__results__result">
     <a href="/posts/a022d31368593358174caf9a5b08f15d29bb4181">https://lenster.xyz</a>
   </div>
+  <img src={imageurl} alt="">
 </div>
 <!---------------------------------------------------------------->
 
--- web-app/src/routes/api/get-img/+server.ts
+++ web-app/src/routes/api/get-img/+server.ts
@@ -1,35 +1,50 @@
-import puppeteer from "puppeteer-core";
+// import puppeteer from "puppeteer-core";
 import { json } from "@sveltejs/kit";
-import { PRIVATE_BROWSERLESS_KEY } from "$env/static/private";
+// import { PRIVATE_BROWSERLESS_KEY } from "$env/static/private";
+import {chromium} from "playwright";
 
-const getBrowser = () => {
-    return puppeteer.connect({ browserWSEndpoint: `wss://chrome.browserless.io?token=${PRIVATE_BROWSERLESS_KEY}` })
-}
-
+// const getBrowser = () => {
+//     return puppeteer.connect({ browserWSEndpoint: `wss://chrome.browserless.io?token=${PRIVATE_BROWSERLESS_KEY}` })
+// }
+//
+//
+// export async function POST(requestEvent) {
+//     const {request} = requestEvent;
+//     const url = await request.json();
+//
+//     let browser;
+//
+//     try {
+//         const browser = await getBrowser();
+//         const page = await browser.newPage();
+//
+//         await page.goto(url);
+//         const screenshot = await page.screenshot();
+//
+//         return json({
+//             image: screenshot
+//         })
+//
+//
+//     } catch (error) {
+//         console.log("failed to take screenshot")
+//     } finally {
+//         if (browser) {
+//             browser.close();
+//         }
+//     }
+// }
 
 export async function POST(requestEvent) {
-    const {request} = requestEvent;
-    const url = await request.json();
+    const browser = await chromium.launch();
 
-    let browser;
+    const page = await browser.newPage();
+    await page.setViewportSize({ width: 1280, height: 1080 });
+    await page.goto("http://nytimes.com");
+    const result = await page.screenshot();
+    await browser.close();
 
-    try {
-        const browser = await getBrowser();
-        const page = await browser.newPage();
-
-        await page.goto(url);
-        const screenshot = await page.screenshot();
-
-        return json({
-            image: screenshot
-        })
-
-
-    } catch (error) {
-        console.log("failed to take screenshot")
-    } finally {
-        if (browser) {
-            browser.close();
-        }
-    }
+    return json({
+        data:result
+    })
 }
