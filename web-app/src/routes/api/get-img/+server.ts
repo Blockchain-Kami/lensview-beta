e02be36e58c3d882e6c350934ff53e40cf75e8c4
parikshitbarua <parikshitb04@gmail.com>
2023-05-24 13:15:09 +0530

    page screenshot using browserless

---
+++ web-app/src/routes/api/get-img/+server.ts
@@ -0,0 +1,35 @@
+import puppeteer from "puppeteer-core";
+import {json} from '@sveltejs/kit';
+import PRIVATE_BROWSERLESS_KEY from "$env/static/private";
+
+const getBrowser = () => {
+    return puppeteer.connect({ browserWSEndpoint: `wss://chrome.browserless.io?token=${PRIVATE_BROWSERLESS_KEY}` })
+}
+
+
+export async function POST(requestEvent) {
+    const {request} = requestEvent;
+    const url = await request.json();
+
+    let browser;
+
+    try {
+        const browser = await getBrowser();
+        const page = await browser.newPage();
+
+        await page.goto(url);
+        const screenshot = await page.screenshot();
+
+        return json({
+            image: screenshot
+        })
+
+
+    } catch (error) {
+        console.log("failed to take screenshot")
+    } finally {
+        if (browser) {
+            browser.close();
+        }
+    }
+}
