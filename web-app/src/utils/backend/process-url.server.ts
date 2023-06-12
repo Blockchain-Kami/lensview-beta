370a0b4bd4f6d301ae8aa4c38e52ffd214c90a70
parikshitbarua <parikshitb04@gmail.com>
2023-06-12 18:30:17 +0530

    Cleaning logic split up and comments added.

--- web-app/src/utils/backend/process-url.server.ts
+++ web-app/src/utils/backend/process-url.server.ts
@@ -1,11 +1,9 @@
 import {URL} from 'url';
+import {cleanURL} from "../clean-url";
 
 /**
- * Preprocess the URL before storing.
- * The URL will be stored according to the follwoing rules:
- *  1. fragments (#) from the URL will be removed.
- *  3. www from the url will be removed.
- *  2. if the URL follows https or http protocol, the URL are stored in the following format
+ * Preprocess and clean the URL before storing.
+ * The URL will be stored according to the following rules:
  *
  *      a. if URL contains query parameters
  *      and ends with '/' (https://github.com/marketplace/?category=chat&type=apps&query=new+/), the trailing '/'
@@ -21,34 +19,33 @@ import {URL} from 'url';
  *      in this case the trailing / will not be removed as this is the standard way of writing the url
  *
  **/
-export const preprocessURL = (url) => {
+export const preprocessURL = (enteredLink) => {
     try {
-        url = url.trim().split('#')[0];
-
-        url = stripWww(url);
-
+        let url = cleanURL(enteredLink);
+        // convert the url to a URL object
         const parsedURL = new URL(url);
-
+        // logic to handle https and http urls
         if (parsedURL['protocol'] === 'https:' || parsedURL['protocol'] === 'http:') {
-
+            // remove extra hash at the end of the pathname, if it exists in the url
             if (parsedURL['pathname'].toString()[parsedURL['pathname'].length - 1] === '/' && parsedURL['pathname'].toString().length > 1) {
                 parsedURL['pathname'] = parsedURL['pathname'].toString().substring(0, parsedURL['pathname'].length - 1);
             }
-
+            // remove extra / from the url
             if (parsedURL['protocol'] === 'https:') {
                 url = parsedURL['protocol'].toString() + '//' + parsedURL['href'].toString().substring(8,).replaceAll(/[/]+/g, '/');
 
             } else if (parsedURL['protocol'] === 'http:') {
-
                 url = parsedURL['protocol'].toString() + '//' + parsedURL['href'].toString().substring(7,).replaceAll(/[/]+/g, '/');
             }
 
-        } else if (parsedURL['protocol'] === 'data:') {
+        }
+        // logic to handle data URIs
+        else if (parsedURL['protocol'] === 'data:') {
             url = parsedURL['href'];
         }
 
         const processedURL = new URL(url);
-
+        // extract the relevant pieces of the url
         const cleanURL = processedURL['href'];
         const origin = processedURL['origin'];
         const path = origin + processedURL['pathname'];
@@ -68,9 +65,3 @@ export const preprocessURL = (url) => {
     }
 };
 
-function stripWww(s) {
-    const regExp = /^(https?:\/\/)?(www\.)?(.*)/;
-    const regExpArr = regExp.exec(s);
-    if (!regExpArr) return s;
-    return `${regExpArr[1] ?? ""}${regExpArr[3]}`
-}
