11159d29d0481430ab0a85af6c9a7dbaba1cc38d
anjaysahoo <anjaysahoo3@gmail.com>
2023-08-01 22:13:07 +0530

    Related pub api integrated

--- web-app/src/routes/search/+page.ts
+++ web-app/src/routes/search/+page.ts
@@ -1,14 +1,22 @@
 import type { PageLoad } from './$types';
 
 export const load = (async () => {
-	return {
-		foundedMainPostPubId: [
-			'0x7e11-0x0136',
-			'0x7e11-0x0158',
-			'0x7e11-0x015c',
-			'0x7e11-0x0155',
-			'0x7e11-0x0142',
-			'0x7e11-0x0140'
-		]
-	};
+	try {
+		const relatedPubs = await fetch('/api/related-pubs', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify('youtube.com')
+		}).then((res) => res.json());
+		console.log('relatedPubs', relatedPubs);
+		return {
+			foundedMainPostPubId: relatedPubs
+		};
+	} catch (error) {
+		console.log('error', error);
+		return {
+			foundedMainPostPubId: []
+		};
+	}
 }) satisfies PageLoad;
