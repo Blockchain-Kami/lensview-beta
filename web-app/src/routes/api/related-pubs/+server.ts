ed10a5f19158e631bae3e53193698b12bae90173
parikshitbarua <parikshitb04@gmail.com>
2023-08-07 23:53:58 +0530

    refactored related-pubs API. API can now search publication IDs for multiple keywords.

--- web-app/src/routes/api/related-pubs/+server.ts
+++ web-app/src/routes/api/related-pubs/+server.ts
@@ -5,20 +5,7 @@ import { createHash } from '../../../utils/backend/sha1.server';
 import { preprocessURL } from '../../../utils/backend/process-url.server';
 import { isInputTypeUrl } from '../../../utils/backend/check-input-type.server';
 
-export async function POST(requestEvent) {
-	const { request } = requestEvent;
-
-	const inputString = await request.json();
-	const URL = isInputTypeUrl(inputString);
-	let tag;
-
-	if (URL) {
-		const [, hostname, ,] = preprocessURL(URL);
-		tag = createHash(hostname);
-	} else {
-		tag = inputString.trim();
-	}
-
+const getRelatedParentPublications = async (tag) => {
 	const posts = await fetch(PUBLIC_LENS_API_URL, {
 		method: 'POST',
 		headers: {
@@ -34,18 +21,47 @@ export async function POST(requestEvent) {
 	});
 
 	const postJSON = await posts.json();
-	const relatedPosts = postJSON.data.publications;
+	return postJSON.data.publications;
+}
 
-	if (relatedPosts.items.length < 1) {
-		throw error(404, {
-			message: 'No related publications found'
-		});
-	}
+
+export async function POST(requestEvent) {
+	const { request } = requestEvent;
+
+	const inputString = await request.json();
+
+	const URL = isInputTypeUrl(inputString);
 	const relatedPubArray = [];
+	let tag;
 
-	for (let i = 0; i < relatedPosts.items.length; i++) {
-		relatedPubArray.push(relatedPosts.items[i]['id']);
+	if (URL) {
+		const [, hostname, ,] = preprocessURL(URL);
+		tag = createHash(hostname);
+
+		const relatedPosts = await getRelatedParentPublications(tag)
+
+		if (relatedPosts.items.length < 1) {
+			throw error(404, {
+				message: 'No related publications found'
+			});
+		}
+
+		for (let i = 0; i < relatedPosts.items.length; i++) {
+			relatedPubArray.push(relatedPosts.items[i]['id']);
+		}
+
+		return json(relatedPubArray);
+
+	} else {
+		const keywords = inputString.split(' ');
+
+		for (let i = 0; i < keywords.length ; i++) {
+			const { items } = await getRelatedParentPublications(keywords[i]);
+
+			items.forEach((tag) => {
+				relatedPubArray.push(tag.id);
+			})
+		}
+		return json(relatedPubArray);
 	}
-
-	return json(relatedPubArray);
 }
