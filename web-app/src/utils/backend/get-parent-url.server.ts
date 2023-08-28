cb26a1e51fe96f3e4e9c16749e4b2d41b92240db
parikshitbarua <parikshitb04@gmail.com>
2023-08-28 12:07:22 +0530

    added winston logger.

--- web-app/src/utils/backend/get-parent-url.server.ts
+++ web-app/src/utils/backend/get-parent-url.server.ts
@@ -1,8 +1,10 @@
 import { error } from '@sveltejs/kit';
 import { PUBLIC_APP_LENS_ID, PUBLIC_LENS_API_URL } from '$env/static/public';
 import getPosts from '../../graphql/getPosts';
+import {logger} from "../../log/logManager";
 
 export const getParentPost = async (hashedURL) => {
+	logger.info("utils/backend: get-parent-url.server.ts :: " + "EXECUTION START: getParentPost");
 	try {
 		const posts = await fetch(PUBLIC_LENS_API_URL, {
 			method: 'POST',
@@ -20,6 +22,7 @@ export const getParentPost = async (hashedURL) => {
 		const response = await posts.json();
 
 		if (response.data.publications.items.length < 1) {
+			logger.info("utils/backend: get-parent-url.server.ts :: " + "EXECUTION END: getParentPost: " + "No related publications found");
 			return {
 				status: 404,
 				parent_post_ID: null,
