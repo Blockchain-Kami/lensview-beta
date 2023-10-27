9e3533cced21737ac061840d007f4bfe450664c4
anjaysahoo <anjaysahoo3@gmail.com>
2023-10-27 18:48:55 +0530

    Codegen setup done

--- backend/src/gql/gql.ts
+++ backend/src/gql/gql.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
-import * as types from './graphql';
-import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+import * as types from "./graphql";
+import { TypedDocumentNode as DocumentNode } from "@graphql-typed-document-node/core";
 
 /**
  * Map of all GraphQL operations in the project.
@@ -13,7 +13,8 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
-    "\n  query Publications($hashedURL: String!, $lensId: ProfileId) {\n    publications(\n      request: {\n        profileId: $lensId\n        publicationTypes: [POST]\n        metadata: { locale: \"en-us\", tags: { oneOf: [$hashedURL] } }\n      }\n    ) {\n      items {\n        ... on Post {\n          id\n        }\n      }\n    }\n  }\n": types.PublicationsDocument,
+  '\n  query Publications($hashedURL: String!, $lensId: ProfileId) {\n    publications(\n      request: {\n        profileId: $lensId\n        publicationTypes: [POST]\n        metadata: { locale: "en-us", tags: { oneOf: [$hashedURL] } }\n      }\n    ) {\n      items {\n        ... on Post {\n          id\n        }\n      }\n    }\n  }\n':
+    types.PublicationsDocument
 };
 
 /**
@@ -33,10 +34,13 @@ export function graphql(source: string): unknown;
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "\n  query Publications($hashedURL: String!, $lensId: ProfileId) {\n    publications(\n      request: {\n        profileId: $lensId\n        publicationTypes: [POST]\n        metadata: { locale: \"en-us\", tags: { oneOf: [$hashedURL] } }\n      }\n    ) {\n      items {\n        ... on Post {\n          id\n        }\n      }\n    }\n  }\n"): (typeof documents)["\n  query Publications($hashedURL: String!, $lensId: ProfileId) {\n    publications(\n      request: {\n        profileId: $lensId\n        publicationTypes: [POST]\n        metadata: { locale: \"en-us\", tags: { oneOf: [$hashedURL] } }\n      }\n    ) {\n      items {\n        ... on Post {\n          id\n        }\n      }\n    }\n  }\n"];
+export function graphql(
+  source: '\n  query Publications($hashedURL: String!, $lensId: ProfileId) {\n    publications(\n      request: {\n        profileId: $lensId\n        publicationTypes: [POST]\n        metadata: { locale: "en-us", tags: { oneOf: [$hashedURL] } }\n      }\n    ) {\n      items {\n        ... on Post {\n          id\n        }\n      }\n    }\n  }\n'
+): (typeof documents)['\n  query Publications($hashedURL: String!, $lensId: ProfileId) {\n    publications(\n      request: {\n        profileId: $lensId\n        publicationTypes: [POST]\n        metadata: { locale: "en-us", tags: { oneOf: [$hashedURL] } }\n      }\n    ) {\n      items {\n        ... on Post {\n          id\n        }\n      }\n    }\n  }\n'];
 
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
 }
 
-export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> =
+  TDocumentNode extends DocumentNode<infer TType, any> ? TType : never;
