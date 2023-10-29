fff859e050faa00203fac16c738462f3a130ef17
anjaysahoo <anjaysahoo3@gmail.com>
2023-10-29 14:16:54 +0530

    feature: added schema.graphql and updated path in codegen.ts

--- backend/codegen.ts
+++ backend/codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: "./src/gql/schema.graphql",
+  schema: "schema.graphql",
   documents: ["src/graphql/*.ts"],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
