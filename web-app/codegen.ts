3c354a72b3ab372b7a10c6945550c965a39bb900
anjaysahoo <anjaysahoo3@gmail.com>
2023-10-29 21:52:34 +0530

    feature: added codegen for Lens graphql

---
+++ web-app/codegen.ts
@@ -0,0 +1,15 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+	schema: 'schema.graphql',
+	documents: ['src/graphql/*.ts'],
+	ignoreNoDocuments: true, // for better experience with the watcher
+	generates: {
+		'./src/gql/': {
+			preset: 'client',
+			plugins: []
+		}
+	}
+};
+
+export default config;
--- web-app/src/utils/frontend/updateReactionForAPost.ts
+++ web-app/src/utils/frontend/updateReactionForAPost.ts
@@ -1,9 +1,10 @@
-import addReaction from '../../graphql/addReaction';
+import addReactionMutation from '../../graphql/add-reaction-mutation.graphql';
 import removeReaction from '../../graphql/removeReaction';
 import { userProfile } from '../../services/profile';
 import { createUserClient } from './createClient';
+import type { ReactionTypes } from '../../gql/graphql';
 
-export const addReactionToAPost = async (publicationId: string, reaction: string) => {
+export const addReactionToAPost = async (publicationId: string, reaction: ReactionTypes) => {
 	try {
 		let profileId;
 		const unsub = userProfile.subscribe((profile: any) => {
@@ -18,7 +19,7 @@ export const addReactionToAPost = async (publicationId: string, reaction: string
 		};
 
 		const client = await createUserClient();
-		return await client.mutation(addReaction, { request }).toPromise();
+		return await client.mutation(addReactionMutation, { request }).toPromise();
 	} catch (err) {
 		console.log(err);
 		throw err;
