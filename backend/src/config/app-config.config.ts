f665ff2d27ef20632f7749df4a88c1f50120430e
anjaysahoo <anjaysahoo3@gmail.com>
2023-11-29 21:49:21 +0530

    fix: allowed origin list updated

--- backend/src/config/app-config.config.ts
+++ backend/src/config/app-config.config.ts
@@ -5,7 +5,7 @@ export const ALLOWED_ORIGINS = {
     "http://localhost:3000",
     "https://cdpn.io",
     "http://localhost:5173",
-    "https://lensview.io/",
-    "https://testnet.lensview.io/"
+    "https://lensview.io",
+    "https://testnet.lensview.io"
   ]
 };
