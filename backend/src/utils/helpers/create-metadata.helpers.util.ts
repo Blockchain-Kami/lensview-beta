3e14d16ba0246fa89b2df335ede9a54417c20ff0
parikshitbarua <parikshitb04@gmail.com>
2023-12-02 05:36:58 +0530

    feature: comment with url image functionality added.

--- backend/src/utils/helpers/create-metadata.helpers.util.ts
+++ backend/src/utils/helpers/create-metadata.helpers.util.ts
@@ -1,4 +1,11 @@
-import { link, MetadataAttributeType, textOnly } from "@lens-protocol/metadata";
+import {
+  image,
+  link,
+  MediaImageMimeType,
+  MetadataAttributeType,
+  MetadataLicenseType,
+  textOnly
+} from "@lens-protocol/metadata";
 import {
   PUBLIC_SOURCE_APP_ID,
   PUBLIC_APP_LENS_HANDLE
@@ -85,5 +92,51 @@ export const createMetaDataForAnonymousComment = (comment: string) => {
     //TODO: Check for below fields usage
     // encryptedWith: PublicationMetadataLitEncryption,
     // hideFromFeed: false,
+  });
+};
+
+export const createMetaDataForImageComment = (urlObj: MetadataObjectModel) => {
+  return image({
+    locale: "en-US",
+    tags: ["dd472d3370b389eb8399ea7c795ca9e76ff0d4d7"], //imagePub
+    appId: PUBLIC_SOURCE_APP_ID,
+    attributes: [
+      {
+        key: "creator",
+        type: MetadataAttributeType.STRING,
+        value: PUBLIC_APP_LENS_HANDLE
+      },
+      {
+        key: "app",
+        type: MetadataAttributeType.STRING,
+        value: PUBLIC_SOURCE_APP_ID
+      },
+      {
+        key: "created on",
+        type: MetadataAttributeType.STRING,
+        value: Date.now().toString()
+      }
+    ],
+    image: {
+      item: `${urlObj.image}`,
+      type: MediaImageMimeType.PNG,
+      altTag: "Me touching grass",
+      license: MetadataLicenseType.CCO
+    },
+    attachments: [
+      {
+        item: `${urlObj.image}`,
+        type: MediaImageMimeType.PNG,
+        altTag: "LensView Publication Image",
+        license: MetadataLicenseType.CCO
+      }
+    ],
+    title: "Post by @testlenviewcode",
+    content: `Image link for the url: ${urlObj.image}`
+    //TODO: Check for below fields usage
+    // content: EncryptableMarkdown
+    // attachments: [PublicationMetadataMediaVideo],
+    // hideFromFeed: true,
+    // encryptedWith: PublicationMetadataLitEncryption,
   });
 };
