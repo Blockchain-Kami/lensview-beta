b63e13f0e1d41a74c9c2fe1c6f2407eb92f50fe5
parikshitbarua <parikshitb04@gmail.com>
2023-12-17 19:19:16 +0530

    feature: anonymous comment with gas.

--- backend/src/utils/publications/comment-onchain.publication.util.ts
+++ backend/src/utils/publications/comment-onchain.publication.util.ts
@@ -3,11 +3,20 @@ import type {
   CreateOnchainCommentBroadcastItemResult,
   OnchainCommentRequest
 } from "../../gql/graphql";
+import {
+  APP_ADDRESS,
+  PUBLIC_LENS_HUB_CONTRACT_ADDRESS,
+  PUBLIC_USE_GASLESS
+} from "../../config/env.config";
 import createOnchainCommentTypedDataService from "../../services/lens/create-onchain-comment-typed-data.lens.service";
 import { signedTypeDataForCommentHelperUtil } from "../helpers/sign-type-data.helper.util";
 import broadcastOnchainRequestService from "../../services/lens/broadcast-onchain-request.lens.service";
 import { waitUntilBroadcastIsCompleteTransactionUtil } from "../transaction/wait-until-broadcast-is-complete.transaction.util";
 import type { RelayError, RelaySuccess } from "../../gql/graphql";
+import { getPolygonGasPriceHelperUtil } from "../helpers/get-polygon-gas-price.helper.utils";
+import { splitSignatureHelperUtil } from "../helpers/split-signature.helper.utils";
+import { createContractHelperUtils } from "../helpers/create-contract.helper.utils";
+import LENS_HUB_ABI from "../../abis/lens-hub-contract.abi.json";
 
 const commentOnChainPublicationUtil = async (
   parentPubId: string,
@@ -16,50 +25,6 @@ const commentOnChainPublicationUtil = async (
   //TODO: Check in production weather we need "@lens-protocol/metadata", if it works putting in
   // devDependencies then keep it or go with schema approach that there in "api-examples" repo
   // https://docs.lens.xyz/docs/publication-metadata#json-schemas
-
-  // const metadata = image({
-  //   locale: "en-US",
-  //   tags: ["dd472d3370b389eb8399ea7c795ca9e76ff0d4d7"], //imagePub
-  //   appId: PUBLIC_SOURCE_APP_ID,
-  //   attributes: [
-  //     {
-  //       key: "creator",
-  //       type: MetadataAttributeType.STRING,
-  //       value: "testlenviewcode"
-  //     },
-  //     {
-  //       key: "app",
-  //       type: MetadataAttributeType.STRING,
-  //       value: "testlenviewcode"
-  //     },
-  //     {
-  //       key: "created on",
-  //       type: MetadataAttributeType.STRING,
-  //       value: Date.now().toString()
-  //     }
-  //   ],
-  //   image: {
-  //     item: "https://ik.imagekit.io/lens/media-snapshot/c5c43e9298b1eb8120e3963244387144132334c9aacddffc0b4900359642897c.jpg",
-  //     type: MediaImageMimeType.PNG,
-  //     altTag: "Me touching grass",
-  //     license: MetadataLicenseType.CCO
-  //   },
-  //   attachments: [
-  //     {
-  //       item: "https://ik.imagekit.io/lens/media-snapshot/c5c43e9298b1eb8120e3963244387144132334c9aacddffc0b4900359642897c.jpg",
-  //       type: MediaImageMimeType.PNG,
-  //       altTag: "Me touching grass",
-  //       license: MetadataLicenseType.CCO
-  //     }
-  //   ],
-  //   title: "Post by @testlenviewcode",
-  //   content: "Image link for the url: ${IPFSLink}"
-  //   //TODO: Check for below fields usage
-  //   // content: EncryptableMarkdown
-  //   // attachments: [PublicationMetadataMediaVideo],
-  //   // hideFromFeed: true,
-  //   // encryptedWith: PublicationMetadataLitEncryption,
-  // });
 
   const ipfsResultUri = await uploadToIPFSHelperUtil(JSON.stringify(metadata));
   console.log("post onchain: ipfs result uri", ipfsResultUri);
@@ -90,41 +55,53 @@ const commentOnChainPublicationUtil = async (
   );
   console.log("comment onchain: signature", signature);
 
-  // if (USE_GASLESS) {
-  const broadcastResult = (await broadcastOnchainRequestService({
-    id,
-    signature
-  })) as RelaySuccess | RelayError;
+  if (PUBLIC_USE_GASLESS) {
+    const broadcastResult = (await broadcastOnchainRequestService({
+      id,
+      signature
+    })) as RelaySuccess | RelayError;
 
-  await waitUntilBroadcastIsCompleteTransactionUtil(broadcastResult, "Comment");
-  // } else {
-  //   const { v, r, s } = splitSignature(signature);
-  //
-  //   const tx = await lensHub.commentWithSig(
-  //     {
-  //       profileId: typedData.value.profileId,
-  //       contentURI: typedData.value.contentURI,
-  //       pointedProfileId: typedData.value.pointedProfileId,
-  //       pointedPubId: typedData.value.pointedPubId,
-  //       referrerProfileIds: typedData.value.referrerProfileIds,
-  //       referrerPubIds: typedData.value.referrerPubIds,
-  //       referenceModuleData: typedData.value.referenceModuleData,
-  //       actionModules: typedData.value.actionModules,
-  //       actionModulesInitDatas: typedData.value.actionModulesInitDatas,
-  //       referenceModule: typedData.value.referenceModule,
-  //       referenceModuleInitData: typedData.value.referenceModuleInitData
-  //     },
-  //     {
-  //       signer: address,
-  //       v,
-  //       r,
-  //       s,
-  //       deadline: typedData.value.deadline
-  //     },
-  //     { gasLimit: 10000000 }
-  //   );
-  //   console.log("comment onchain: tx hash", tx.hash);
-  // }
+    await waitUntilBroadcastIsCompleteTransactionUtil(
+      broadcastResult,
+      "Comment"
+    );
+  } else {
+    const polygonGasFee = await getPolygonGasPriceHelperUtil();
+    const { v, r, s } = splitSignatureHelperUtil(signature);
+
+    const lensHub = createContractHelperUtils(
+      PUBLIC_LENS_HUB_CONTRACT_ADDRESS,
+      LENS_HUB_ABI
+    );
+
+    const tx = await lensHub.commentWithSig(
+      {
+        profileId: typedData.value.profileId,
+        contentURI: typedData.value.contentURI,
+        pointedProfileId: typedData.value.pointedProfileId,
+        pointedPubId: typedData.value.pointedPubId,
+        referrerProfileIds: typedData.value.referrerProfileIds,
+        referrerPubIds: typedData.value.referrerPubIds,
+        referenceModuleData: typedData.value.referenceModuleData,
+        actionModules: typedData.value.actionModules,
+        actionModulesInitDatas: typedData.value.actionModulesInitDatas,
+        referenceModule: typedData.value.referenceModule,
+        referenceModuleInitData: typedData.value.referenceModuleInitData
+      },
+      {
+        signer: APP_ADDRESS,
+        v,
+        r,
+        s,
+        deadline: typedData.value.deadline
+      },
+      {
+        maxFeePerGas: polygonGasFee.maxFeePerGas,
+        maxPriorityFeePerGas: polygonGasFee.maxPriorityFeePerGas
+      }
+    );
+    console.log("comment onchain: tx hash", tx.hash);
+  }
 };
 
 export default commentOnChainPublicationUtil;
