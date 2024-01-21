6fb1a92ed982f5a297f17888fed7b14d5b3c675b
anjaysahoo <anjaysahoo3@gmail.com>
2024-01-21 21:14:15 +0530

    chore:86cucrwbg-long name

--- web-app/src/components/posts-page/CommentsOfAPublication.svelte
+++ web-app/src/components/posts-page/CommentsOfAPublication.svelte
@@ -321,7 +321,8 @@
               <div class="CenterRowFlex comment__body__top__left">
                 {#if comment?.by?.metadata?.displayName !== undefined && comment?.by?.metadata?.displayName !== null}
                   <div class="comment__body__top__left__name">
-                    {comment?.by?.metadata?.displayName}
+                    {comment?.by?.metadata?.displayName.substring(0, 5)}
+                    {#if comment?.by?.metadata?.displayName.length >= 5}..{/if}
                   </div>
                   <div class="comment__body__top__left__dot" />
                 {/if}
