f0d68cb78026ef793bf4f4222909acf47dc251c8
Anjay Sahoo <65654701+anjaysahoo@users.noreply.github.com>
2024-05-19 19:05:49 +0530

    ➕feature: added navigation bar
    - Integrated tooltip
    - Post & notification tab added

---
+++ extension/src/config/app-constants.config.ts
@@ -0,0 +1,4 @@
+export enum NavItems {
+  post = "POST",
+  notification = "NOTIFICATION"
+}
---
+++ extension/src/popup/posts/Posts.svelte
@@ -0,0 +1,5 @@
+<h1>
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet laborum
+  maxime temporibus. Ab aut beatae eligendi harum ipsa nisi quasi quisquam sunt
+  suscipit ullam. Consequuntur dolores iusto molestias placeat temporibus?
+</h1>
