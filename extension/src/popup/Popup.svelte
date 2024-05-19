7bde6f08dd7c06d6e682f850128f1ca35619b73d
Anjay Sahoo <65654701+anjaysahoo@users.noreply.github.com>
2024-05-19 10:05:30 +0530

    ➕feature: added graphql setup

--- extension/src/popup/Popup.svelte
+++ extension/src/popup/Popup.svelte
@@ -1,4 +1,18 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+  import getChallengeInfoLensService from "../services/lens/get-challenge-info.lens.service";
+
+  onMount(() => {
+    test();
+  });
+
+  const test = async () => {
+    const challenge = await getChallengeInfoLensService(
+      "0xa03dbDA93D960BBDa97B9a2F74729de186C22638",
+      "0x018776"
+    );
+    console.log("challenge: ", challenge);
+  };
 </script>
 
 <main>
